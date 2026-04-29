8671689c28428b64b17a71a8f186b5454e9b81ed
cecildh0 <cecildh0@sewanee.edu>
2026-04-29 14:21:42 -0500

    Initial commit for the solar system simulation site.

---
+++ main.js
@@ -0,0 +1,564 @@
+/*-----------------------------------------------------------------
+ * Final Project 
+ * Miniature solar system
+ * 
+ * Updated additions:
+ * *Imported fire particle effect param for planets, makes planet look hot from a distance
+ * *Added ability to toggle spinning of planets with spacebar
+ * *Added ability to drag earth orbit with shift+click and drag
+ * *Additionally, movmement of other plants and moons are simulated togehter with this user-inputted movement of the earth
+ * Full solar system is implemented
+ * *Press X to toggle whether pluto is included.
+ *  It's up to you if pluto's a planet
+ * Ring transparency param 
+ * 
+ * Created by Daniel Cecil
+ * three.particle.fire by yomotsu
+ * Planet textures from https://www.solarsystemscope.com/textures/
+ *  
+ * Updated 4/22/2026
+ *---------------------------------------------------------------*/
+
+//import three js and all the addons that are used in this script 
+import * as THREE from 'three';
+import { TW } from 'tw';
+import GUI from 'gui';
+import particleFire from 'three-particle-fire';
+import { makePlanetParams } from './planet.js';
+
+particleFire.install( { THREE: THREE } );
+
+console.log(`Loaded Three.js version ${THREE.REVISION}`);
+
+// for debugging
+globalThis.THREE = THREE;
+globalThis.TW = TW;
+
+// Create an initial empty Scene
+var scene = new THREE.Scene();
+globalThis.scene = scene;
+
+// set scene background to stars.jpg
+const loader = new THREE.TextureLoader();
+loader.load('stars.jpg', function(texture) {
+    texture.wrapS = THREE.MirroredRepeatWrapping;
+    texture.wrapT = THREE.MirroredRepeatWrapping;
+    texture.repeat.set(2, 2);
+    scene.background = texture;
+});
+
+// ================================================================
+// Build your scene here
+
+const earthParams = {
+    name: 'earth',
+    radius: 2.4,
+    color: 0x3366ff,
+    texture: "earth.png",
+    ring: false,
+    moons: 1,
+    moonColor: 0x888888,
+    moonTexture: "moon.png",
+    firey: false,
+};
+
+const marsParams = {
+    name: 'mars',
+    radius: 1.3,
+    color: 0xff5522,
+    texture: "mars.jpg",
+    ring: false,
+    moons: 2,
+    moonColor: 0x999999,
+    moonTexture: "moon.png",
+    firey: false,
+};
+
+const saturnParams = {
+    name: 'saturn',
+    radius: 5.9,
+    color: 0xffcc88,
+    texture: "saturn.jpg",
+    ring: true,
+    ringColor: 0x996633,
+    ringTexture: "saturn_ring.png",
+    ringTransparency: 1,
+    moons: 285,
+    moonColor: 0xaaaaaa,
+    moonTexture: "moon.png",
+    firey: false,
+};
+
+const sunParams = {
+    name: 'sun',
+    radius: 18,
+    color: 0xffff66,
+    texture: "sun.jpg",
+    ring: false,
+    moons: 0,
+    lighting: true,
+    lightColor: 0xfad2af,
+    moonTexture: "moon.png",
+    firey: true,
+};
+
+const mercuryParams = {
+    name: 'mercury',
+    radius: 0.9,
+    color: 0x888888,
+    texture: "mercury.jpg",
+    ring: false,
+    moons: 0,
+    lighting: false,
+    lightColor: 0x888888,
+    moonTexture: "moon.png",
+    firey: false,
+}
+
+const venusParams = {
+    name: 'venus',
+    radius: 2.2,
+    color: 0xffaa88,
+    texture: "venus.jpg",
+    ring: false,
+    moons: 0,
+    lighting: false,
+    lightColor: 0xffaa88,
+    moonTexture: "moon.png",
+    firey: false,
+}
+
+const jupiterParams = {
+    name: 'jupiter',
+    radius: 7.0,
+    color: 0xffff66,
+    texture: "jupiter.jpg",
+    ring: true, //will implement transparent ring later
+    ringColor: 0x7a3e2b,
+    ringTransparency: 0.15,
+    moons: 101,
+    moonColor: 0xaaaaaa,
+    moonTexture: "moon.png",
+    firey: false,
+}
+
+const uranusParams = {
+    name: 'uranus',
+    radius: 3.1,
+    color: 0x888888,
+    texture: "uranus.jpg",
+    ring: true, //will implement transparent ring later
+    ringColor: 0x2b2b2b,
+    ringTransparency: 0.15,
+    moons: 28,
+    moonColor: 0xaaaaaa,
+    moonTexture: "moon.png",
+    firey: false,
+}
+
+const neptuneParams = {
+    name: 'neptune',
+    radius: 3.0,
+    color: 0x888888,
+    texture: "neptune.jpg",
+    ring: true, //will implement transparent ring later
+    ringColor: 0x3b2f2f,
+    ringTransparency: 0.15,
+    moons: 16,
+    moonColor: 0xaaaaaa,
+    moonTexture: "moon.png",
+    firey: false,
+}
+
+const plutoParams = {
+    name: 'pluto',
+    radius: 0.4,
+    color: 0xaaaaaa,
+    texture: "pluto.jpg",
+    ring: false,
+    moons: 0,
+    firey: false,
+}
+// Globals to hold planet groups for organization. Extra globals can store extra meshes if necessary for spinning or other animations
+var sun;
+var sunFireUp;
+var sunFireDown;
+
+// Distances are based on AU proportions, compressed with sqrt(AU) for visibility.
+const mercuryDistance = 23;
+var mercuryGroup;
+
+const venusDistance = 27;
+var venusGroup;
+
+const earthDistance = 30;
+var earthGroup;
+
+const marsDistance = 38;
+var marsGroup;
+
+const jupiterDistance = 57;
+var jupiterGroup;
+
+const saturnDistance = 72;
+var saturnGroup;
+
+const uranusDistance = 95;
+var uranusGroup;
+
+const neptuneDistance = 115;
+var neptuneGroup;
+
+const plutoDistance = 125;
+var plutoGroup;
+
+
+// earth's mesh
+var earthMesh;
+
+
+// pivots for rotating planets around the sun
+var mercuryPivot;
+var venusPivot;
+var earthPivot;
+var marsPivot;
+var jupiterPivot;
+var saturnPivot;
+var uranusPivot;
+var neptunePivot;
+var plutoPivot;
+
+// Orbit rates about the sun
+// I honestly dont know how fast every planet spins around the sun so like if I have time to research that I'll
+// make it more accurate but I honestly just dont think that part is very interesting
+const mercuryOrbitSpeed = 0.7;
+const venusOrbitSpeed = 0.6;
+const earthOrbitSpeed = 0.8;
+const marsOrbitSpeed = 0.5;
+const jupiterOrbitSpeed = 0.3;
+const saturnOrbitSpeed = 0.2;
+const uranusOrbitSpeed = 0.15;
+const neptuneOrbitSpeed = 0.1;
+const plutoOrbitSpeed = 0.05;
+// Spin speeds for planets
+const mercurySpinSpeed = 0.4;
+const venusSpinSpeed = 0.3;
+const earthSpinSpeed = 0.5;
+const marsSpinSpeed = 0.3;
+const jupiterSpinSpeed = 0.3;
+const saturnSpinSpeed = 0.2;
+const uranusSpinSpeed = 0.2;
+const neptuneSpinSpeed = 0.1;
+var plutoSpinSpeed = 0.05;
+// Self-spin speed for Earth's mesh (slightly slower than the group). 
+// Only the earth's axis is seperated because I think it looks better when its spin is seperate from its moon
+const earthSelfSpinSpeed = 0.25;
+
+// Fixed time-step, treated as seconds
+const simDt = 0.016;
+
+//Toggle spinning off planets
+var planetsSpinning = true;
+
+// Check if earth is being dragged
+var draggingEarthOrbit = false;
+
+//Toggle pluto
+var plutoExists = false;
+
+//To be honest the whole remaking thing isnt totally necessary unless I 
+// decide to add a GUI element or something. But I want to leave the door open for
+// that
+function remakeSolarSystem() {
+    // This just removes pivots but Im not really worried about adding and removing right now so whatever
+    if (sun) scene.remove(sun);
+    if (mercuryPivot) scene.remove(mercuryPivot);
+    if (venusPivot) scene.remove(venusPivot);
+    if (earthPivot) scene.remove(earthPivot);
+    if (marsPivot) scene.remove(marsPivot);
+    if (jupiterPivot) scene.remove(jupiterPivot);
+    if (saturnPivot) scene.remove(saturnPivot);
+    if (uranusPivot) scene.remove(uranusPivot);
+    if (neptunePivot) scene.remove(neptunePivot);
+    if(plutoGroup) scene.remove(plutoGroup);
+    if (plutoPivot) scene.remove(plutoPivot);
+
+    // Sun
+    makePlanetParams(scene, sunParams);
+    sun = scene.getObjectByName('sun');
+    //sun particles
+    sunFireUp = sun.getObjectByName('sun_fireUp');
+    sunFireDown = sun.getObjectByName('sun_fireDown');
+
+
+    // Mercury
+    mercuryGroup = new THREE.Object3D();
+    mercuryGroup.position.set(mercuryDistance, 0, 0);
+    makePlanetParams(mercuryGroup, mercuryParams);
+    mercuryPivot = new THREE.Object3D();
+    mercuryPivot.add(mercuryGroup);
+    scene.add(mercuryPivot);
+
+    // Venus
+    venusGroup = new THREE.Object3D();
+    venusGroup.position.set(venusDistance, 0, 0);
+    makePlanetParams(venusGroup, venusParams);
+    venusPivot = new THREE.Object3D();
+    venusPivot.add(venusGroup);
+    scene.add(venusPivot);
+
+    // Earth 
+    earthGroup = new THREE.Object3D();
+    earthGroup.position.set(earthDistance, 0, 0);
+    makePlanetParams(earthGroup, earthParams);
+    earthMesh = earthGroup.getObjectByName(earthParams.name);
+    earthPivot = new THREE.Object3D();
+    earthPivot.add(earthGroup);
+    scene.add(earthPivot);
+
+    // Mars
+    marsGroup = new THREE.Object3D();
+    marsGroup.position.set(marsDistance, 0, 0);
+    makePlanetParams(marsGroup, marsParams);
+    marsPivot = new THREE.Object3D();
+    marsPivot.add(marsGroup);
+    scene.add(marsPivot);
+
+    // Jupiter
+    jupiterGroup = new THREE.Object3D();
+    jupiterGroup.position.set(jupiterDistance, 0, 0);
+    makePlanetParams(jupiterGroup, jupiterParams);
+    jupiterPivot = new THREE.Object3D();
+    jupiterPivot.add(jupiterGroup);
+    scene.add(jupiterPivot);
+
+    // Saturn
+    saturnGroup = new THREE.Object3D();
+    saturnGroup.position.set(saturnDistance, 0, 0);
+    makePlanetParams(saturnGroup, saturnParams);
+    saturnPivot = new THREE.Object3D();
+    saturnPivot.add(saturnGroup);
+    scene.add(saturnPivot);
+
+    // Uranus
+    uranusGroup = new THREE.Object3D();
+    uranusGroup.position.set(uranusDistance, 0, 0);
+    makePlanetParams(uranusGroup, uranusParams);
+    uranusPivot = new THREE.Object3D();
+    uranusPivot.add(uranusGroup);
+    scene.add(uranusPivot);
+
+    // Neptune
+    neptuneGroup = new THREE.Object3D();
+    neptuneGroup.position.set(neptuneDistance, 0, 0);
+    makePlanetParams(neptuneGroup, neptuneParams);
+    neptunePivot = new THREE.Object3D();
+    neptunePivot.add(neptuneGroup);
+    scene.add(neptunePivot);
+
+    //Pluto
+    plutoGroup = new THREE.Object3D();
+    plutoGroup.position.set(plutoDistance, 0, 0);
+    makePlanetParams(plutoGroup, plutoParams);
+    plutoPivot = new THREE.Object3D();
+    plutoPivot.add(plutoGroup);
+    plutoPivot.visible = plutoExists;
+    scene.add(plutoPivot);
+
+}
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambientLight);
+
+// const directionalLight = new THREE.DirectionalLight(0xffffff, 2.0);
+// directionalLight.position.set(1, 1, 3);
+// scene.add(directionalLight);
+
+// initial setup
+remakeSolarSystem();
+
+// ================================================================
+
+// Create a renderer to render the scene
+var renderer = new THREE.WebGLRenderer();
+
+// TW.mainInit() initializes TW, adds the canvas to the document,
+// enables display of 3D coordinate axes, sets up keyboard controls
+TW.mainInit(renderer,scene);
+
+// const gui = new GUI();
+// gui.add(params, 'hatHeight', 1, 10).onChange(remakeFigure);
+// gui.add(params, 'hatAngle', -Math.PI/2, +Math.PI/2).onChange(remakeFigure);
+// gui.add(params, 'headDim', 1, 10).onChange(remakeFigure);
+// gui.add(params, 'bodyHeight', 1, 20).onChange(remakeFigure);
+// gui.add(params, 'bodyWidth', 1, 10).onChange(remakeFigure);
+// gui.addColor(params, 'bodyColor').onChange((val) => body.material.color.setHex(val));
+// gui.addColor(params, 'headColor').onChange(remakeFigure);
+// gui.addColor(params, 'faceColor').onChange(remakeFigure);
+// gui.addColor(params, 'hatColor').onChange((val) => hat.material.color.setHex(val));
+
+
+// Set up a camera for the scene
+var state = TW.cameraSetup(renderer,
+                           scene,
+                           // This is really big lol
+                           {minx: -130, maxx: +130,
+                            miny: -30, maxy: 90,
+                            minz: -130, maxz: +130});
+
+// TW sets near from scene size (~30); orbit zoom can move the camera inside that and clip the sun and particles.
+if (state.cameraObject) {
+    state.cameraObject.near = 0.1;
+    state.cameraObject.updateProjectionMatrix();
+}
+
+// Continue to next (or previous) frame of animation
+// Either if animation is unpaused or if the earth is being moved by user
+function advanceSimulation(dt, planetsOnly) {
+    if (planetsOnly) {
+        if (mercuryPivot) mercuryPivot.rotation.y += mercuryOrbitSpeed * dt;
+        if (venusPivot) venusPivot.rotation.y += venusOrbitSpeed * dt;
+        if (earthPivot) earthPivot.rotation.y += earthOrbitSpeed * dt;
+        if (marsPivot) marsPivot.rotation.y += marsOrbitSpeed * dt;
+        if (jupiterPivot) jupiterPivot.rotation.y += jupiterOrbitSpeed * dt;
+        if (saturnPivot) saturnPivot.rotation.y += saturnOrbitSpeed * dt;
+        if (uranusPivot) uranusPivot.rotation.y += uranusOrbitSpeed * dt;
+        if (neptunePivot) neptunePivot.rotation.y += neptuneOrbitSpeed * dt;
+        if (plutoPivot && plutoExists) plutoPivot.rotation.y += plutoOrbitSpeed * dt;
+
+        if (mercuryGroup) mercuryGroup.rotation.y += mercurySpinSpeed * dt;
+        if (venusGroup) venusGroup.rotation.y += venusSpinSpeed * dt;
+        if (earthGroup) earthGroup.rotation.y += earthSpinSpeed * dt;
+        if (earthMesh) earthMesh.rotation.y += earthSelfSpinSpeed * dt;
+        if (marsGroup) marsGroup.rotation.y += marsSpinSpeed * dt;
+        if (jupiterGroup) jupiterGroup.rotation.y += jupiterSpinSpeed * dt;
+        if (saturnGroup) saturnGroup.rotation.y += saturnSpinSpeed * dt;
+        if (uranusGroup) uranusGroup.rotation.y += uranusSpinSpeed * dt;
+        if (neptuneGroup) neptuneGroup.rotation.y += neptuneSpinSpeed * dt;
+        if (plutoGroup && plutoExists) plutoGroup.rotation.y += plutoSpinSpeed * dt;
+
+    }
+    if (sunFireUp && sunFireDown) {
+        sunFireUp.material.update(dt);
+        sunFireDown.material.update(dt);
+    }
+}
+
+// Animation for unpaused, do next frame and continue until the animation is stopped
+function animate() {
+    requestAnimationFrame(animate);
+    if (!draggingEarthOrbit) {
+        if (planetsSpinning) {
+            advanceSimulation(simDt, true);
+        } else {
+            advanceSimulation(simDt, false);
+        }
+    }
+}
+animate();
+
+
+
+
+// ================================================================
+// mouse handling: Shift + drag on Earth 
+
+// Sensitivity of earth orbit dragging
+const earthOrbitDragSensitivity = 0.004;
+
+const raycaster = new THREE.Raycaster();
+
+// Convert mouse position to NDC, for raycasting
+function convertMousePositionToNDC(event, domElement) {
+    const rect = domElement.getBoundingClientRect();
+    const xNDC = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+    const yNDC = -((event.clientY - rect.top) / rect.height) * 2 + 1;
+    return new THREE.Vector2(xNDC, yNDC);
+}
+
+// Check if the earth mesh is being hit by the mouse
+function pickEarthMesh(ndc) {
+    if (!earthMesh || !state.cameraObject) return null;
+    raycaster.setFromCamera(ndc, state.cameraObject);
+    const hits = raycaster.intersectObject(earthMesh, false);
+    return hits.length > 0 ? hits[0] : null;
+}
+
+// Set the orbit controls enabled or disabled
+function setOrbitControlsEnabled(enabled) {
+    if (state.cameraControls) state.cameraControls.enabled = enabled;
+}
+
+// Check if the earth mesh is clicked and held by the mouse
+function onPointerDownEarth(evt) {
+    if (evt.button !== 0 || !evt.shiftKey) return;
+    const canvas = renderer.domElement;
+    if (evt.target !== canvas) return;
+    const ndc = convertMousePositionToNDC(evt, canvas);
+    if (!pickEarthMesh(ndc)) return;
+    draggingEarthOrbit = true;
+    canvas.setPointerCapture(evt.pointerId);
+    setOrbitControlsEnabled(false);
+}
+
+// Move the earth orbit (and all other movements corresponding to where the earth would be if it were spinning)
+function onPointerMoveEarth(evt) {
+    if (!draggingEarthOrbit || !earthPivot) return;
+    const dThetaEarth = evt.movementX * earthOrbitDragSensitivity;
+    // Simulate all orbits and spins together
+    const dSimTime = dThetaEarth / earthOrbitSpeed;
+    advanceSimulation(dSimTime, true);
+}
+
+// Stop dragging the earth orbit
+function onPointerUpEarth(evt) {
+    if (!draggingEarthOrbit) return;
+    draggingEarthOrbit = false;
+    const canvas = renderer.domElement;
+    if (canvas.hasPointerCapture(evt.pointerId)) {
+        canvas.releasePointerCapture(evt.pointerId);
+    }
+    setOrbitControlsEnabled(true);
+}
+
+// Toggle spinning of planets with spacebar
+function onKeyDownToggleSpin(evt) {
+    if (evt.code !== 'Space' && evt.key !== ' ') return;
+    if (evt.repeat) return;
+    evt.preventDefault();
+    planetsSpinning = !planetsSpinning;
+}
+
+// Toggle Pluto visibility with X key
+function onKeyDownTogglePluto(evt) {
+    if (evt.code !== 'KeyX' && evt.key.toLowerCase() !== 'x') return;
+    if (evt.repeat) return;
+    plutoExists = !plutoExists;
+    if (plutoPivot) plutoPivot.visible = plutoExists;
+}
+
+const canvas = renderer.domElement;
+canvas.addEventListener('pointerdown', onPointerDownEarth);
+canvas.addEventListener('pointermove', onPointerMoveEarth);
+canvas.addEventListener('pointerup', onPointerUpEarth);
+canvas.addEventListener('pointercancel', onPointerUpEarth);
+window.addEventListener('keydown', onKeyDownToggleSpin);
+window.addEventListener('keydown', onKeyDownTogglePluto);
+
+// ================================================================
+
+// window.addEventListener( 'resize', function () {
+
+//   var width  = window.innerWidth;
+//   var height = window.innerHeight;
+
+//   camera.aspect = width / height;
+//   camera.updateProjectionMatrix();
+//   renderer.setSize( width, height );
+//   if (sunFireUp && sunFireDown) {
+//     sunFireUp.material.setPerspective( camera.fov, height );
+//     sunFireDown.material.setPerspective( camera.fov, height );
+//   }
+
+// } );
+
---
+++ planet.js
@@ -0,0 +1,213 @@
+
+import * as THREE from 'three';
+import particleFire from 'three-particle-fire'; 
+
+
+/* Simple planet module.
+
+Default parameters (example):
+const params = {
+    radius: 4,            // radius of planet
+    color: 0x3366ff,       // color of planet, used if texture is not provided
+    texture: undefined,    // optional: filepath to planet texture image
+
+    ring: false,           // true to add a ring
+    ringColor: 0x888888,   // color of ring, used if ringTexture not provided
+    ringTexture: undefined, // optional: filepath to ring texture image
+    ringTransparency: 1 // transparency of ring. 0 is invisible, 1 is visible
+
+    moons: 0,              // number of moons to add
+    moonColor: 0x888888,   // color of moon, used if moonTexture not provided
+    moonTexture: undefined, // optional: filepath to moon texture image
+
+    lighting: false,       // true to add emissive glow and point light to planet
+    lightColor: 0xffffff,     // used if lighting is true   
+
+    firey: false,           // true to add a fire particle effect to the planet 
+};
+
+This creates a planet scaled to "radius". If "texture" (string for file path) is provided it is used the texure, otherwise "color" is used.
+If "ring" is true a flat ring is added slightly below the planet's equatorial plane (only a bit below for lighting purposes). Ring material may be a color or a texture, depending on if "ringTexture" is provided.
+If "moons" > 0, that many moons are added around the planet. Moon material may be a color or a texture, depending on if "moonTexture" is provided.
+If "lighting" is true, an emissive glow is added to the planet material, and a point light is added at the planet's position. The color of the light is determined by "lightColor".
+
+Notes:
+- The planet's local origin is at the planet's center.
+- The planets's natural size (excluding ring or moons) is determined by "radius"
+- The function adds the planet (and any rings/moons/lights) to the parent, and returns that parent
+- Example usage:
+    makePlanetParams(scene, { name: "earth", radius: 4, texture: "earth.png", moons: 1 });
+    scene.add(group);
+*/
+
+function makePlanetParams(parent, params) {
+    const r = params.radius;
+    const planetGeom = new THREE.SphereGeometry(1, 32, 32);
+
+    let planetMat;
+    if (params.texture) {
+        const loader = new THREE.TextureLoader();
+        planetMat = new THREE.MeshPhongMaterial({ map: loader.load(params.texture) });
+    } else {
+        planetMat = new THREE.MeshPhongMaterial({ color: params.color });
+    }
+
+    const planet = new THREE.Mesh(planetGeom, planetMat);
+    planet.scale.set(r, r, r);
+    
+    planet.name = params.name || 'planet';
+    
+    parent.add(planet);
+    
+    if (params.firey) {
+        // Fire particle effect from three-particle-fire 
+        const fireRadius = r * 0.06;     
+        const fireHeight = r * 0.2;       
+        const particleCount = 20000;       
+        
+        const fireGeometry = new particleFire.Geometry(fireRadius, fireHeight, particleCount);
+        const fireMaterial = new particleFire.Material(0xffefc4);
+        // I dont really think the depthTest works
+        fireMaterial.depthTest = false;
+        var fireParticle1 = new THREE.Points( fireGeometry, fireMaterial );
+        const fireGeometry2 = new particleFire.Geometry(fireRadius, -fireHeight, particleCount);
+        var fireParticle2 = new THREE.Points( fireGeometry2, fireMaterial );
+        //Same with this stuff
+        fireParticle1.renderOrder = 1;
+        fireParticle2.renderOrder = 1;
+        fireParticle1.frustumCulled = false;
+        fireParticle2.frustumCulled = false;
+        
+        fireParticle1.name = (params.name || 'planet') + '_fireUp';
+        fireParticle2.name = (params.name || 'planet') + '_fireDown';
+        planet.add(fireParticle1);
+        planet.add(fireParticle2);
+    }
+
+    //lighting
+    if (params.lighting) {
+        // simple emissive glow on the planet material
+        if (planet.material) {
+            // if a texture is provided, use it as an emissive map so the texture 'glows'
+            if (params.texture) {
+                const loader = new THREE.TextureLoader();
+                planet.material.emissiveMap = loader.load(params.texture);
+                planet.material.emissive = new THREE.Color(params.lightColor);
+            } else {
+                planet.material.emissive = new THREE.Color(params.color);
+            }
+            // // make the emissive visible and give a stronger glow
+            planet.material.emissiveIntensity = 1.5;
+            // planet.material.needsUpdate = true;
+        }
+        // strong point light to reinforce the glow
+        const light = new THREE.PointLight(params.lightColor, 5000, 0);
+        light.position.copy(planet.position);
+        parent.add(light);
+    }
+
+    //ring
+    if (params.ring) {
+        const inner = r * 1.1;
+        const outer = r * 2.0;
+        const ringGeom = new THREE.RingGeometry(inner, outer, 64);
+        const ringOpacity = params.ringTransparency;
+        var ringIsTransparent;
+        if (ringOpacity < 1) {
+            ringIsTransparent = true;
+        } else {
+            ringIsTransparent = false;
+        }
+
+        const pos = ringGeom.attributes.position;
+        const uv = ringGeom.attributes.uv;
+        for (let i = 0; i < pos.count; i++) {
+            const x = pos.getX(i);
+            const y = pos.getY(i);
+            const dist = Math.sqrt(x * x + y * y);
+            uv.setY(i, (dist - inner) / (outer - inner)); // radial
+            uv.setX(i, Math.atan2(y, x) / (2 * Math.PI) + 0.5); // angular
+        }
+        uv.needsUpdate = true;
+
+        let ringMat;
+        if (params.ringTexture) {
+            const ringTex = new THREE.TextureLoader().load(params.ringTexture);
+            //ringTex.wrapS = THREE.RepeatWrapping;
+            //ringTex.wrapT = THREE.RepeatWrapping;
+            ringMat = new THREE.MeshPhongMaterial({
+                map: ringTex,
+                side: THREE.DoubleSide,
+                transparent: ringIsTransparent,
+                opacity: ringOpacity
+            });
+        } else {
+            ringMat = new THREE.MeshPhongMaterial({
+                color: params.ringColor,
+                side: THREE.DoubleSide,
+                transparent: ringIsTransparent,
+                opacity: ringOpacity
+            });
+        }
+        const ring = new THREE.Mesh(ringGeom, ringMat);
+        // ring in the XZ plane (flat around Y axis)
+        ring.rotateX(Math.PI / 2);
+        // lower the ring slightly so the sun's light actually affects it
+        ring.position.y = -r * 0.05;
+        ring.name = (params.name || 'planet') + '_ring';
+        parent.add(ring);
+    }
+
+    //moons
+    if (params.moons && params.moons > 0) {
+        const moonsGroup = new THREE.Object3D();
+        moonsGroup.name = (params.name || 'planet') + '_moons';
+
+        //If a planet has a lot of moons, draw a moon belt instead
+        const manyMoons = params.moons > 12;
+
+        //If the moons are too big its hard to tell a moon apart from a planet
+        const maxMoonRadius = 0.5;
+        for (let i = 0; i < params.moons; i++) {
+            let mr = Math.min(r * 0.25, maxMoonRadius); // Cap
+            if (manyMoons) mr = Math.min(r * 0.12, 0.2); // smaller for belt look
+            const moonGeom = new THREE.SphereGeometry(1, 10, 10);
+            let moonMat;
+            if (params.moonTexture) {
+                moonMat = new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load(params.moonTexture) });
+            } else {
+                moonMat = new THREE.MeshPhongMaterial({ color: params.moonColor || 0x888888 });
+            }
+            const moon = new THREE.Mesh(moonGeom, moonMat);
+            moon.scale.set(mr, mr, mr);
+            // Moons are in a circle around XZ plane.
+            let distance;
+            if (params.ring) {
+                distance = r * 2.5;
+            } else {
+                distance = r * 1.1;
+            }
+            if (manyMoons) {
+                const beltInner = distance * 0.8;
+                const beltOuter = distance * 1.2;
+                const angle = Math.random() * Math.PI * 2;
+                const beltDistance = beltInner + Math.random() * (beltOuter - beltInner);
+                moon.position.set(beltDistance * Math.cos(angle), 0, beltDistance * Math.sin(angle));
+            } else {
+                const angle = (i / params.moons) * Math.PI * 2;
+                moon.position.set(distance * Math.cos(angle), 0, distance * Math.sin(angle));
+            }
+            // Moons numbered
+            moon.name = (params.name || 'planet') + '_moon' + i;
+            moonsGroup.add(moon);
+        }
+        
+        parent.add(moonsGroup);
+    }
+
+    return parent;
+}
+
+
+
+export { makePlanetParams };
